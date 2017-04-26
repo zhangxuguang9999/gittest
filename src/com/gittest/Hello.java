97bf83ba7c5cc8092b52acd5353558221c053d87
zhangxuguang <1132896070@qq.com>
2017-04-26 22:16:11 +0800

    test

--- src/com/gittest/Hello.java
+++ src/com/gittest/Hello.java
@@ -2,7 +2,7 @@ package com.gittest;
 
 public class Hello {
 	public Hello(){
-		System.out.println("this is tests for push to github throught ssh protocl");
+		System.out.println("this is tests for push to github throught ssh protocl j");
 		
 	}
 }
